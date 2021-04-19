316066384ab023dff9758a36dd6997f517f46706
xqgdtx1 <xqgdtx1@gmail.com>
2021-04-20 01:20:00 +0800

    看似解决了启动不了UserCenterApplication 的bug

--- user-center/src/main/java/com/vaynenet/user/UserCenterApplication.java
+++ user-center/src/main/java/com/vaynenet/user/UserCenterApplication.java
@@ -3,7 +3,7 @@ package com.vaynenet.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.vaynenet.user.mapper"})
 public class UserCenterApplication {
 
     public static void main(String[] args) {
