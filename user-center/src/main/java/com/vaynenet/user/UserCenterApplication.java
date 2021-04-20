2b6d00c80008b06c858581f25b38c0ff6485b657
joe <xqgdtx1@sina.com>
2021-04-20 15:06:13 +0800

    提交更新

--- user-center/src/main/java/com/vaynenet/user/UserCenterApplication.java
+++ user-center/src/main/java/com/vaynenet/user/UserCenterApplication.java
@@ -1,13 +1,26 @@
 package com.vaynenet.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"com.vaynenet.user.mapper"})
+import java.util.Arrays;
+
+@EnableScheduling
+@SpringBootApplication(scanBasePackages = {"com.vaynenet.user","com.vaynenet.core.lock"})
+@MapperScan(basePackages = {"com.vaynenet.user.mapper"})
 public class UserCenterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserCenterApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(UserCenterApplication.class, args);
+        String[] beans = ctx.getBeanDefinitionNames();
+        Arrays.sort(beans);
+        for (String bean : beans) {
+            System.out.println(bean + " of Type :: " + ctx.getBean(bean).getClass());
+        }
+
     }
 
 }
---
+++ vn-swagger-starter/src/main/java/com/vaynenet/swagger2/VnSwaggerStarterApplication.java
@@ -0,0 +1,11 @@
+package com.vaynenet.swagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnSwaggerStarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VnSwaggerStarterApplication.class, args);
+    }
+}
